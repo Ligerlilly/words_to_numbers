08174a8eb75f766cb5a1ba358d43f4b46904066f
Jason Lane & Lindsay Culver <student@epicodus.com>
2015-08-04 12:00:36 -0700

    translates one and two digit numbers into words

---
+++ lib/numbers_to_words.rb
@@ -0,0 +1,60 @@
+class Fixnum
+    define_method(:to_w) do
+    ones_hash = {
+      1 => 'one',
+      2 => 'two',
+      3 => 'three',
+      4 => 'four',
+      5 => 'five',
+      6 => 'six',
+      7 => 'seven',
+      8 => 'eight',
+      9 => 'nine'
+     }
+     tens_hash = {
+       1 => 'ten',
+       2 => 'twenty',
+       3 => 'thirty',
+       4 => 'fourty',
+       5 => 'fifty',
+       6 => 'sixty',
+       7 => 'seventy',
+       8 => 'eighty',
+       9 => 'ninety'
+     }
+     numbers_array = self.to_s.split('')
+     count = 0
+     first_word = ''
+     second_word = ''
+     third_word = ''
+     fourth_word = ''
+     if numbers_array.length == 1
+       first_word = ones_hash[numbers_array[0].to_i]
+     elsif numbers_array.length == 3
+       numbers_array.each do |num|
+         count += 1
+         if count == 1
+           first_word = ones_hash[num.to_i]
+         elsif count == 2
+           third_word = tens_hash[num.to_i]
+         elsif count == 3
+           fourth_word = ones_hash[num.to_i]
+         end
+
+       end
+       second_word = "hundred"
+     else
+       numbers_array.each do |num|
+         count += 1
+         if count == 1
+           first_word = tens_hash[num.to_i]
+         elsif count == 2
+           second_word = ones_hash[num.to_i]
+         end
+
+       end
+     end
+    words = "#{first_word}" + " " + "#{second_word}" + " " + "#{third_word}"  + " " + "#{fourth_word}"
+    words.rstrip
+    end
+end
---
+++ spec/numbers_to_words_spec.rb
@@ -0,0 +1,14 @@
+require 'rspec'
+require 'numbers_to_words'
+require 'pry'
+
+describe Fixnum do
+  it "translates one digit numbers into written form" do
+    expect(1.to_w()).to eq("one")
+  end
+
+  it "translates two didgit numbers into written form" do
+    expect(25.to_w).to eq("twenty five")
+  end
+
+end
